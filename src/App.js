6461c212079d1336b8d647732decd502a3fe0ad7
Shivam Bhardwaj <bshiam472@gmail.com>
2025-04-07 02:02:56 +0530

    analytics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Body from './components/Body';
 import { Provider } from 'react-redux';
 import appStore from './utils/appStore';
+import { Analytics } from "@vercel/analytics/react"
 function App() {
   return (
     <Provider store={appStore}>
